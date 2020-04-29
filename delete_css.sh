3f0e601544f8e53ffec3447c9a3cf2f75e6afb96
Pablo Alviggi <pablo.alviggi@flagbit.de>
2020-04-29 11:22:01 +0200

    delete_css can delete a single less file

--- delete_css.sh
+++ delete_css.sh
@@ -1,4 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+files=${1:-*}
+
+#if [[ $files -eq '-h' ]] ; then
+#    echo 'Usage: delete_css [less filename without ext]'
+#	echo 'It will remove all the compiled css files and less intermediate files'
+#	echo 'if a name is given, it will remove the compiled css and only the less indicated'
+#   exit 0
+#fi
+
+
+
 find pub -name styles-?.* -exec rm {} +
 find var -name styles-?.* -exec rm {} +
-find var -name *.less -exec rm {} +
+find var -name $files.less -exec rm {} +
